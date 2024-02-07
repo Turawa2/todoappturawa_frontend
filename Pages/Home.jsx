f104077320d866d189869b88af0668a339382e60
Turawa <hamzamusaturawa@gmail.com>
2024-02-07 00:56:29 -0800

    initial comit

---
+++ Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+<>
+<h1>React TODO APP</h1>
+
+</>
+    )
+}
+
+export default Home
--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  const API_URL = "http://localhost:5038/";
+
+  useEffect(() => {
+    refreshNotes();
+  }, []);
+
+  const refreshNotes = () => {
+    fetch(API_URL + "api/todoapp/getAll")
+      .then((response) => response.json())
+      .then((data) => {
+        setNotes(data);
+      });
+  };
+
+  const addClick = () => {
+    const newNotes = document.getElementById("newNotes").value;
+    const data = new FormData();
+    data.append("newNotes", newNotes);
+
+    fetch(API_URL + "api/todoapp/addNote", {
+      method: "POST",
+      body: data,
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        alert(result);
+        refreshNotes();
+      });
+  };
+
+  const deleteClick = (id) => {
+    fetch(API_URL + "api/todoapp/deleteNote?id=" + id, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        alert(result);
+        refreshNotes();
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+      <h1>Todo App</h1>
+      <input id="newNotes" /> &ensp;
+      <button onClick={addClick}>Add Note</button>
+      {notes.map((note) => (
+        <p key={note.id}>
+          <b>* {note.description}</b>&ensp;
+          <button onClick={() => deleteClick(note.id)}>Delete Note</button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      ))}
+    </>
   );
 }
 
